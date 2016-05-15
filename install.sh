5cb823934274565e06b5fe9e8720ec4b94c56fbf
Sindre Sivertsen <sindrejohan1@gmail.com>
2016-05-15 13:32:05 +0200

    fixed backup/cp bug

--- install.sh
+++ install.sh
@@ -1,31 +1,40 @@
-#!/bin/bash
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="bashrc vimrc  zshrc  dmenurc Xresources gtkrc-2.0"
+folders="oh-my-zsh vim config"
 
-########## Variables
+# create backup folder
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
-
-
-
-##########
-
-# create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+	echo "------"
+	echo "Copy $file  existing dotfiles from ~ to $olddir"
+	mv ~/.$file ~/dotfiles_old
+	echo "Creating symlink to $file in home directory"
+	ln -s $dir/$file ~/.$file
 done
+
+for folder in $folders; do
+	echo "------------"
+	echo "backup $folder folder"
+	cp -r ~/.$folder $olddir
+	echo "merge and overviret $folder to home"
+	mkdir -p ~/.$folder
+	cp -r $dir/$folder/* ~/.$folder
+done
+
+echo "Copy fonts"
+echo "-----------------------------"
+sudo cp $dir/fonts/* /usr/share/fonts/TTF/
+# update fonts cache
+fc-cache
+
+echo "copy Scripts"
+sudo cp -r $dir/scripts ~/scripts
