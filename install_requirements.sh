5f9240c14ed4e1962599e587981f6477b0583b06
Sindre Sivertsen <sindrejohan1@gmail.com>
2016-05-15 14:56:47 +0200

    add vundle

---
+++ install_requirements.sh
@@ -0,0 +1,21 @@
+pacman=" gnome-themes-standard 
+gtk-engine-murrine
+rxvt-unicode
+feh
+compton"
+
+pacaur="i3-gaps
+gtk-theme-arc
+numix-icon-theme-git
+xcursor-chameleon-pearl
+i3blocks-gaps-git
+rofi-git
+pulseaudio
+pulseaudio-ctl
+i3lock-color-git
+j4-dmenu-desktop
+vundle"
+pip="the-fuck"
+sudo pacman -S $pacman
+yaourt -S --noconfirm $pacaur
+pip install $pip
