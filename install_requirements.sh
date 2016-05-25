00a0631c27e10634dd0d0009ecc976afa693cb2d
Sindre Sivertsen <sindrejohan1@gmail.com>
2016-05-26 00:12:20 +0200

    made laptop changes

--- install_requirements.sh
+++ install_requirements.sh
@@ -22,7 +22,7 @@ pulseaudio-ctl
 i3lock-color-git
 j4-dmenu-desktop
 vundle
-i3-lock-fancy-dualmonitors-git"
+i3-lock-fancy-git"
 pip="the-fuck"
 sudo pacman -S $pacman
 yaourt -S --noconfirm $pacaur
