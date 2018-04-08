900bf939f9b45fae69c7d51b86857550f30796ab
nikzy <sindrejohan1@gmail.com>
2018-04-08 14:03:36 +0200

    added backup script

---
+++ scripts/backup_toNAS.sh
@@ -0,0 +1,3 @@
+rsync -aAxv --password-file pw_nas.txt --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/archie/.cache","/var/cache"} / admin@192.168.1.12::Qweb/backup_arch-home
+ 
+
