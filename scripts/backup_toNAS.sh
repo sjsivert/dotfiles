76766345a630ee96958a329343a2e7bfd62d8f99
Sindre Johan Imenes Sivertsen <sindrejohan@live.no>
2018-04-28 12:55:54 +0200

    Merge branch 'master' into origin/laptop

--- scripts/backup_toNAS.sh
+++ scripts/backup_toNAS.sh
@@ -1,3 +1,3 @@
-rsync -aAxv --password-file pw_nas.txt --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/archie/.cache","/var/cache"} / admin@192.168.1.12::Qweb/backup_arch-home
+rsync -aAxv --password-file ~/scripts/pw_nas.txt --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/archie/.cache","/var/cache"} / admin@192.168.1.12::Qweb/backup_arch-home
  
 
---
+++ scripts/film_back.sh
@@ -0,0 +1,1 @@
+rsync -aAxv --password-file ~/scripts/pw_nas.txt /run/media/archie/13EDAA954410DDB3/ admin@192.168.1.12::Qweb/milla_dimmeFilm
