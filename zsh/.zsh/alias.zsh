c5af2ce4f6ea067c0009994d349dbbb1b3d6a16c
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-08-10 00:14:16 +0200

    Add nice vim to vscode and bunch of other stuff

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -21,3 +21,8 @@ alias vim='nvim'
 alias todo='topydo columns'
 alias wall='QuickWall'
 alias walb='wal -i ~/.QuickWall -b "#1D232F"'
+alias td="topydo"
+alias gap="git add -p"
+# Go over and fix
+alias co= "!git for-each-ref --format='%(refname:short)' refs/heads | fzf | xargs git checkout"
+alias standup='{git -C ~/dev/capra/handlekonto-frontend log --since 1.day --author sindrejohan1@gmail.no; git -C ~/dev/capra/coopay-handlekonto-backend-springboot log --since 1.day --author sindrejohan1@gmail.com} | cat'
