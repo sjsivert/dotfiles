d679520f4583e09a02f6f5ac8d8cec54dcce9895
Sindre Sivertsen <sindrejohan1@gmail.com>
2024-02-22 13:21:46 +0100

    chore: enable marking untracked files under VCS as dirty for faster repository status check

--- zsh/.zsh/oh-my-zsh.zsh
+++ zsh/.zsh/oh-my-zsh.zsh
@@ -54,7 +54,7 @@ BULLETTRAIN_PROMPT_ORDER=(
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
 # much, much faster.
- DISABLE_UNTRACKED_FILES_DIRTY="true"
+ DISABLE_UNTRACKED_FILES_DIRTY="false"
 
 
 
