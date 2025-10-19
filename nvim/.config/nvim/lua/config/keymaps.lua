5cd774c551fe9a7853b84d7937b6fb5a1e397e7c
Sindre Sivertsen <sindrejohan1@gmail.com>
2025-10-19 16:39:42 +0200

    vim: esc is now jk kj aswell

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local g = vim.g
+local set = vim.api.nvim_set_keymap
+local ns = { noremap = true, silent = true }
+
+set("i", "jk", "<Esc>", ns)
+set("i", "kj", "<Esc>", ns)
