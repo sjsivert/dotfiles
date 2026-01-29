2c7a072793362344f8e62311e67ba0f855290a7c
Sindre Sivertsen <sindrejohan1@gmail.com>
2026-01-29 09:07:45 +0100

    nvim: Update config

--- nvim/.config/nvim/lua/plugins/example.lua
+++ nvim/.config/nvim/lua/plugins/example.lua
@@ -44,13 +44,13 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
+            -- add a keymap to browse plugin files
+            -- stylua: ignore
+            {
+                "<leader>fp",
+                function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+                desc = "Find Plugin File",
+            },
     },
     -- change some options
     opts = {
@@ -83,8 +83,9 @@ return {
       "jose-elias-alvarez/typescript.nvim",
       init = function()
         require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+                    -- stylua: ignore
+                    vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports",
+                        { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
         end)
       end,
@@ -132,6 +133,7 @@ return {
         "regex",
         "tsx",
         "typescript",
+        "svelte",
         "vim",
         "yaml",
       },
@@ -141,16 +143,16 @@ return {
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-      })
-    end,
-  },
+  -- {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     opts = function(_, opts)
+  --         -- add tsx and treesitter
+  --         vim.list_extend(opts.ensure_installed, {
+  --             "tsx",
+  --             "typescript",
+  --         })
+  --     end,
+  -- },
 
   -- the opts function can also be used to change the default opts:
   {
@@ -184,7 +186,7 @@ return {
 
   -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "stylua",
